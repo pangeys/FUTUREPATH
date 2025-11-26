bbc7b0b870d93a7cc84d9e21b159a803b6db24cc
RELDA69 <153437284+RELDA69@users.noreply.github.com>
2025-11-26 15:42:21 +0800

    added signup.js

--- login.js
+++ login.js
@@ -11,9 +11,6 @@ document.getElementById("loginForm").addEventListener("submit", function(e) {
     if(user === correctUser && pass === correctPass) {
         alert('Login successful!');
     }   
-    if(user == "" && pass == ""){
-        alert('Please input Username and Password!');
-    }
     else if(user !== correctUser || pass !== correct){ 
         alert('Incorect username or password.');
     }  
